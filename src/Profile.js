30156eaee1eed68bd652c8d38a3bba2b21aa9898
Ryan Lee <rmlee002@gmail.com>
2019-04-14 00:17:18 -0400

    Backend to retrieve data

--- src/Profile.js
+++ src/Profile.js
@@ -2,11 +2,30 @@ import React, { Component } from 'react'
 export default class ProfilePage extends Component {
     constructor(props) {
         super(props)
+        this.state ={
+            data: []
+        }
     }
+
+    componentDidMount(){
+        const options = { decrypt: false }
+        this.userSession.getFile(this.userSession.loadUserData().username + ".json", options)
+        .then((content) => {
+            if(content) {
+                const data = JSON.parse(content)
+            } else {            
+                console.log("Error retrieving user data")
+                alert("Error retrieving user data")
+            }
+        })
+    }
+
     render(){
         return (
-            <h1>Profile Page</h1>
-            
+            <React.Fragment>
+                <h1>Profile Page</h1>
+                <h2>{typeof this.state.data}</h2>
+            </React.Fragment>
         )
     }
 }
