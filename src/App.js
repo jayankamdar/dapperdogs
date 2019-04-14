587e3c0617d0905cdcace15b295f35c564b88a31
Ryan Lee <rmlee002@gmail.com>
2019-04-14 07:47:08 -0400

    FOrmatting

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import { UserSession } from 'blockstack'
-// import UserInfo from './UserInfo';
 import Landing from './Landing';
 import SignedIn from './SignedIn';
-
-// const blockstack = require('blockstack');
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.userSession = new UserSession()
-    //   let isSignedIn = this.checkSignedInStatus();
-
-    //   this.handleSignIn = this.handleSignIn.bind(this)
-    //   this.handleSignOut = this.handleSignOut.bind(this)
   }
-
-  // checkSignedInStatus() {
-  //   if (blockstack.isUserSignedIn()) {
-  //     return true;
-  //   } else if (blockstack.isSignInPending()) {
-  //     blockstack.handlePendingSignIn().then(function(userData) {
-  //       window.location = window.location.origin
-  //     })
-  //     return false;
-  //   }
-  // }
-
-  // handleSignIn(event) {
-  //   event.preventDefault();
-  //   blockstack.redirectToSignIn()
-  // }
-
-  // handleSignOut(event) {
-  //   event.preventDefault();
-  //   blockstack.signUserOut(window.location.href)
-  // }
 
   componentDidMount() {
     const session = this.userSession
