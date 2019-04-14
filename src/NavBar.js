ef67f783346af49b0c39d002126976ebd013e6a2
johnm216 <johnm216@terpmail.umd.edu>
2019-04-13 20:28:24 -0400

    changed create/edit to two seperate pages

--- src/NavBar.js
+++ src/NavBar.js
@@ -22,7 +22,10 @@ class NavBar extends Component {
       <Link className="nav-link" to="/profile">View Profile</Link>
     </li>
     <li className="nav-item">
-      <Link className="nav-link" to="/others">Create/Edit Profile</Link>
+      <Link className="nav-link" to="/create">Create Profile</Link>
+    </li>
+    <li className="nav-item">
+      <Link className="nav-link" to="/edit">Edit Profile</Link>
     </li>
   </ul>
 </div>
