8eaeb242e8761aaa44ad549bd93fd79c4b7f0f48
Ryan Lee <rmlee002@gmail.com>
2019-04-14 02:00:30 -0400

    Formatting

--- src/SignedIn.js
+++ src/SignedIn.js
@@ -1,46 +1,20 @@
 import React, { Component } from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom'
-import { UserSession, Profile } from 'blockstack'
+import { Switch, Route } from 'react-router-dom'
+import { UserSession } from 'blockstack'
 import NavBar from './NavBar'
-import Landing from './Landing'
 import { appConfig} from './constants'
 import './SignedIn.css'
 import ProfilePage from './Profile'
 import CreatePage from './Create'
-
+import Feed from './Feed'
 
 class SignedIn extends Component {
 
   constructor(props) {
     super(props)
-    this.state = {
-      me: {},
-      savingMe: false,
-      savingKingdown: false,
-      redirectToMe: false
-    }
 
-	this.userSession = new UserSession({ appConfig })
-    this.loadMe = this.loadMe.bind(this)
-    this.saveMe = this.saveMe.bind(this)
+  	this.userSession = new UserSession({ appConfig })
     this.signOut = this.signOut.bind(this)
-  }
-
-  componentWillMount() {
-    this.loadMe()
-  }
-
-  loadMe() {
-    const options = { decrypt: false }
-
-  }
-
-  saveMe(me) {
-    this.setState({me, savingMe: true})
-    const options = { encrypt: false }
-    .finally(() => {
-      this.setState({savingMe: false})
-    })
   }
 
   signOut(e) {
@@ -51,41 +25,25 @@ class SignedIn extends Component {
 
   render() {
     const username = this.userSession.loadUserData().username
-    const me = this.state.me
-    const redirectToMe = this.state.redirectToMe
-    if(redirectToMe) {
-      // User hasn't configured her animal
-      if(window.location.pathname !== '/me') {
-        return (
-          <Redirect to="/me" />
-        )
-      }
-    }
 
     return (
       <div className="SignedIn">
-      <NavBar username={username} signOut={this.signOut}/>
-      <Switch>
-              <Route
-                path='/'
-                exact
-                render={
-                  routeProps => <Landing
-                  myKingdom={false}
-                  protocol={routeProps.match.params.protocol}
-                  currentUsername={username}
-                  {...routeProps} />
-                }
-              />
-              <Route
-                path='/profile'
-                component={ProfilePage}
-              />
-			  <Route
-				path='/create'
-				component={CreatePage}
-			  />
-      </Switch>
+        <NavBar username={username} signOut={this.signOut}/>
+        <Switch>
+                <Route
+                  path='/'
+                  exact
+                  component={Feed}
+                />
+                <Route
+                  path='/profile'
+                  component={ProfilePage}
+                />
+          <Route
+          path='/create'
+          component={CreatePage}
+          />
+        </Switch>
       </div>
     );
   }
